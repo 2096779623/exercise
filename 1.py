33241b3839b73adac1a199592de33201cb4e7442
2096779623 <admin@utermux.dev>
2022-06-11 20:02:26 +0800

    two commit
    Signed-off-by: 2096779623 <admin@utermux.dev>

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 '''
 @File    :   1.py
-@Time    :   2022/06/11 19:54:09
+@Time    :   2022/06/11 19:59:16
 @Author  :   2096779623 
 @Contact :   admin@utermux.dev
 @License :   MIT
---
+++ 2.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   2.py
+@Time    :   2022/06/11 19:59:34
+@Author  :   2096779623 
+@Contact :   admin@utermux.dev
+@License :   MIT
+'''
+
+import turtle as te
+te.Pen()
+#te.hideturtle()
+te.pencolor("peru")
+te.pensize(20)
+te.forward(100)
+te.pencolor("seagreen")
+te.pensize(80)
+te.forward(100)
+te.done()
