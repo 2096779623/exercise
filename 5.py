da14fb574116ee1dd5d2e9b69845a5e3ea284110
2096779623 <2096779623@qq.com>
2022-06-19 20:53:07 +0800

    xxx
    Signed-off-by: 2096779623 <2096779623@qq.com>

---
+++ 5.py
@@ -0,0 +1,25 @@
+import turtle as te
+mypen = te.Pen()
+
+mypen.forward(-150)
+mypen.pensize(40)
+mypen.pencolor("tan")
+mypen.forward(100)
+mypen.pencolor("peru")
+mypen.pensize(60)
+mypen.forward(100)
+mypen.pencolor("chocolate")
+mypen.pensize(80)
+mypen.forward(100)
+
+#镜片
+mypen.pencolor("skyblue")
+mypen.pensize(80)
+
+mypen.forward(0)
+mypen.pencolor("lightcyan")
+mypen.pensize(60)
+mypen.forward(0)
+
+mypen.hideturtle()
+te.done()
